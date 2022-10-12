83c6cc93b3f0c516ea69364deea9215e5e454cad
Talgat Iskindir <talgat.iskindir@travelperk.com>
2022-10-12 21:44:50 +0200

    Ingredient Model

--- app/recipe/models.py
+++ app/recipe/models.py
@@ -7,3 +7,14 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    recipe = models.ForeignKey(
+        'Recipe',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
