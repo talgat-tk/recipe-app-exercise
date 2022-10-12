1832e0b00889ee2c556f0ef36253daafa966f2cd
Talgat Iskindir <talgat.iskindir@travelperk.com>
2022-10-12 23:47:32 +0200

    Create Recipe with Ingredients API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,10 +1,34 @@
 from rest_framework import serializers
 
-from recipe.models import Recipe
+from recipe.models import (
+    Recipe,
+    Ingredient,
+)
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ['name']
 
 
 class RecipeSerializer(serializers.ModelSerializer):
+    ingredients = IngredientSerializer(many=True, required=False)
+
     class Meta:
         model = Recipe
-        fields = ['id', 'name', 'description']
+        fields = ['id', 'name', 'description', 'ingredients']
         read_only_fields = ['id']
+
+    def create(self, validated_data):
+        ingredients_data = validated_data.pop('ingredients', [])
+
+        recipe = Recipe.objects.create(**validated_data)
+
+        for ingredient_data in ingredients_data:
+            Ingredient.objects.create(
+                recipe=recipe,
+                **ingredient_data
+            )
+
+        return recipe
