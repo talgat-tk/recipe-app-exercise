2e2dbaeadb9175419d7c9c75bffc5d90780c974b
Talgat Iskindir <talgat.iskindir@travelperk.com>
2022-10-25 14:12:38 +0200

    Revert "Add CORS headers for Frontend"
    This reverts commit ca536cdef45587d39edd686646125253e4555687.

--- app/app/settings.py
+++ app/app/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
@@ -39,16 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'corsheaders',
     'drf_spectacular',
-
     'recipe',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
